4de21d901797ee996ace8e901c1d5c0e1ecdfea3
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-04-15 23:06:54 -0400

    Commit inicial

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan =require('morgan');
+const routes = require('./routes/index')
+const path = require('path')
+const app = express();
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}))  //Para que express pueda entender los datos que son enviados por un formulario
+app.use(express.json());
+// Routes
+app.use(routes);
+
+// Static Content
+app.use(express.static(path.join(__dirname,'public')))
+
+app.listen(5000,()=>console.log('Servidor en puerto 5000'))
