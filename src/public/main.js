4def88246c510f14079ceb730c54e13d81d28a9e
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-04-17 19:51:39 -0400

    Registrando nuestro service-worker de notificaciones

---
+++ src/public/main.js
@@ -0,0 +1,25 @@
+const { parse } = require("dotenv/types");
+
+const PUBLIC_VAPID_KEY = 'BDbsXufIH3eaXp8IE0wcgC28dksesLVvSLu9njWJZqRt0oNdwSh45zO7uKz4YqH3RSZyWeNVLhkXuq4cQXCaT9Y'
+
+const subscription = async()=>{
+    // Service worker
+    const register = await navigator.serviceWorker.register('/service-worker.js',{
+        scope:'/'
+    });
+    console.log('Nuevo service-worker');
+    const subscription = await register.pushManager.subscribe({
+        userVisibleOnly:true,
+        applicationServerKey:PUBLIC_VAPID_KEY
+    });
+    await fetch('/subscription',{
+        method:'POST',
+        body:JSON.stringify(subscription),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    });
+    console.log('Suscrito');
+}
+
+subscription();
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,14 @@ const {Router} = require('express');
 
 const webpush = require('../webpush');
 const router = Router();
+let pushSubscription;
+router.post( '/subscription',(req,res)=>{
+    pushSubscription = req.body;
+    res.status(200).json({
+        ok:true
+    })
+});
+
+
 
 module.exports= router;
