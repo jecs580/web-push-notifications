b4d61c820acc7273dd2bd87e4edb5dad1e023e76
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-04-19 21:00:07 -0400

    Agregando un formulario con input para recibirlo como mensaje en la notificacion

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,24 @@
-const {Router} = require('express');
+const { Router } = require('express');
 
 const webpush = require('../webpush');
 const router = Router();
 let pushSubscription;
-router.post( '/subscription',async (req,res)=>{
+router.post('/subscription', async (req, res) => {
     pushSubscription = req.body;
-    res.status(200).json({ok:true})
-    const payload =  JSON.stringify({
-        title:'Mi notificacion personalizada',
-        message:'Hola mundo'
+    res.status(200).json({ ok: true })
+});
+router.post('/new-message', async (req, res) => {
+    const { message } = req.body;
+    const payload = JSON.stringify({
+        title: 'Jecs Notification',
+        message:message
     })
     try {
-        await webpush.sendNotification(pushSubscription,payload);
+        await webpush.sendNotification(pushSubscription, payload);
     } catch (error) {
         console.log(error);
     }
-});
+})
 
 
-
-module.exports= router;
+module.exports = router;
